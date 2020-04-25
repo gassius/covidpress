8d024e02438b385f06d42f6828160a227172a903
Carlos González Rico <cgonzalezr@gmail.com>
2020-04-25 20:20:35 +0200

    preliminar display of first datasource

--- includes/templates/advisor.php
+++ includes/templates/advisor.php
@@ -3,6 +3,8 @@
  * COVIDPress Notice
  */
 
+ $data = $this->getData()
+
 ?>
 
-<div class="covidpress-advisor bottom theme-ligth">COVID ADVISOR</div>
+<div class="covidpress-advisor bottom theme-ligth">COVID ADVISOR <?=  'Region: '. $data['region'] . ' - '.'Cases: '. number_format($data['cases']) . ' - '.'Deaths: '. number_format($data['deaths']) ?></div>
