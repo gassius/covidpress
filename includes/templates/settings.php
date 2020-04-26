6c083e7020063ccb8e58f24cd73dba8b88cd0083
Carlos González Rico <cgonzalezr@gmail.com>
2020-04-26 19:48:59 +0200

    adjusting settings page

--- includes/templates/settings.php
+++ includes/templates/settings.php
@@ -8,4 +8,18 @@
 <div class="wrap covidpress">
     <h1>COVIDPress Settings</h1>
     <hr class="wp-header-end">
+    <div>
+        <p>
+            Thank you for using COVIDPress. This plugin allows you to show COVID-19 information to your visitors without complex adjustments.
+            This is a first release, developed during the Hackaton <a href='https://eunitedvsvirus.devpost.com/' target="_blank">#EUvrsVid</a>, between April 24th and 26th. 2020.
+        </p>
+        <p>
+            We expect to release updates to this plugin that will allow you, as a site administrator, to customize how is displayed and where, among other improvements.
+            In the meantime, we will appreciate any comment and ideas. Visit our development contact points at 
+            <a href='https://github.com/gassius/covidpress' target="_blank">GitHub</a> (main repo) and <a href='https://devpost.com/software/covidpress' target="_blank">Devpost.</a>
+        </p>
+        <p>
+            Thanks for collaborating in the fight against COVID-19.
+        </p>
+    </div>
 </div>
